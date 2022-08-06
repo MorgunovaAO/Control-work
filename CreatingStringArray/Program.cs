5e6b7d90cad7a4c4d5dcc2dbd4d6d7cc4527ef48
Anastasia <nast2209@yandex.ru>
2022-08-06 16:46:57 +0300

    Add program code

--- CreatingStringArray/Program.cs
+++ CreatingStringArray/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] CreateArray (int length)  
+{  
+    string[] array = new string[length]; 
+    Console.WriteLine("Creating array here: ");  
+    for (int i = 0; i < length; i++)  
+    {  
+        Console.Write($"Input {i + 1} element: "); 
+        array[i] = Console.ReadLine();  
+    }  
+    Console.WriteLine();  
+    return array;  
+}  
+
+void ShowArray(string[] array) 
+{  
+    for (int i = 0; i < array.Length; i++)  
+        Console.Write($"'{array[i]}' ");  
+    Console.WriteLine(); 
+}  
+
+void ShowNewArray(string[] newArray) 
+{  
+    for (int i = 0; i < newArray.Length; i++)  
+        if (newArray[i].Length <= 3) 
+            Console.Write($"'{newArray[i]}' "); 
+    Console.WriteLine(); 
+}  
+
+Console.Write("Input size of array: ");  
+int size = Convert.ToInt32(Console.ReadLine());  
+string[] firstArray = CreateArray(size); 
+
+Console.WriteLine("Your array:");
+ShowArray(firstArray); 
+Console.WriteLine(); 
+
+Console.WriteLine("New array:"); 
+ShowNewArray(firstArray);
